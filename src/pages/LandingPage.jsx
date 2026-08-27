8dad1861733d99c14fd192f7472c95b4d4395bed
Olawale <olawalesodiq988@gmail.com>
2026-08-28 00:12:57 +0100

    Implement ErrorBoundary for enhanced error handling in the application

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,45 +1,46 @@
 import { Hero } from "../Layout/Homepage/Hero";
 import { Cards } from "../Layout/Homepage/Cards";
 import { ProductCard } from "../Layout/Homepage/ProductCard";
-import  CategoryCard  from "../Layout/Homepage/CategoryCard";
+import CategoryCard from "../Layout/Homepage/CategoryCard";
 import { HowToOrder } from "../Layout/Homepage/HowToOrder";
 import { FaqSection } from "../Layout/Homepage/FaqSection";
 import { CustomOrderCTA } from "../Layout/Homepage/CustomOrderCTA";
 import AnimatedSection from "../components/ui/AnimatedSection";
-
-
+// import ErrorBoundary from "../components/ErrorBoundary";
 
 export const HomePage = () => {
   return (
-    <>
-      <AnimatedSection>
-        <Hero />
-      </AnimatedSection>
+    // <ErrorBoundary>
+      <>
+        <AnimatedSection>
+          <Hero />
+        </AnimatedSection>
 
-      <AnimatedSection>
-        <CategoryCard />
-      </AnimatedSection>
+        <AnimatedSection>
+          <CategoryCard />
+        </AnimatedSection>
 
-      <AnimatedSection>
-        <ProductCa />
-      </AnimatedSection>
+        <AnimatedSection>
+          <ProductCard />
+        </AnimatedSection>
 
-      <AnimatedSection>
-        <Cards />
-      </AnimatedSection>
+        <AnimatedSection>
+          <Cards />
+        </AnimatedSection>
 
-      <AnimatedSection>
-        <HowToOrder />
-      </AnimatedSection>
+        <AnimatedSection>
+          <HowToOrder />
+        </AnimatedSection>
 
-      <AnimatedSection>
-        <FaqSection />
-      </AnimatedSection>
+        <AnimatedSection>
+          <FaqSection />
+        </AnimatedSection>
 
-      <AnimatedSection>
-        <CustomOrderCTA />
-      </AnimatedSection>
-    </>
+        <AnimatedSection>
+          <CustomOrderCTA />
+        </AnimatedSection>
+      </>
+    // </ErrorBoundary>
   );
 };
 
